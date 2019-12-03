ddee2c5e09a337cf8ff9b7e6116d99b844953b4c
DexterWuskie <57875768+DexterWuskie@users.noreply.github.com>
2019-12-03 20:26:24 +0000

    Add files via upload

--- profController.java
+++ profController.java
@@ -1,7 +1,8 @@
 package application;
-
 import java.io.IOException;
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,13 +12,20 @@ import javafx.stage.Stage;
 
 public class profController {
     @FXML
-    private Button btnBack;    
-    
+    private Button btnBack;
     private void initialize() {
-
+    	ObservableList<String> profile = listView.getItems();
+    	try (BufferedReader reader = new BufferedReader(new FileReader(new File("Profiles.txt")))) {
+            String profileToRead;
+            while ((profileToRead = reader.readLine()) != null)
+            	profile.add(profileToRead);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public void fillProfile() throws IOException {
+}
+
+	public void fillProfile() throws IOException {
 		/*fills labels once a book is selected from booksLst
 		bookImage.setImage(Book.getImage());
 		bookTitleLbl.setText("Title - " + Book.getTitle());
@@ -27,15 +35,15 @@ public class profController {
 		bookLangLbl.setText("Language - " + Book.getLanguage());
 		bookISBNLbl.setText("ISBN - " + Book.getISBN());*/
 	}
-    @FXML
- 	private void openMainMenu() throws IOException {
-     	Stage oldStage = (Stage) btnBack.getScene().getWindow();
-     	oldStage.close();
- 		Parent root2 = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
- 		Stage stage = new Stage();
- 		stage.setTitle("Main Menu");
- 		stage.setScene(new Scene(root2));
- 		
- 		stage.show();  
- 	}
+	@FXML
+	private void openMainMenu() throws IOException {
+		Stage oldStage = (Stage) btnBack.getScene().getWindow();
+		oldStage.close();
+			Parent root2 = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("Main Menu");
+			stage.setScene(new Scene(root2));
+
+			stage.show();
+	}
 }
