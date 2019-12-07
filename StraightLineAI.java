d81299aca5d5b5730621a354bc978b2b2d314ad5
Mugwump-A <57906851+Mugwump-A@users.noreply.github.com>
2019-12-07 17:36:54 +0000

    Update StraightLineAI.java

--- StraightLineAI.java
+++ StraightLineAI.java
@@ -5,7 +5,6 @@ public class StraightLineAI extends Enemy{
 	public StraightLineAI(int xCoord, int yCoord, int dy, int dx, Map map) {
 		super(xCoord, yCoord, dy, dx, map);
 		changeDirection();
-		// TODO Auto-generated constructor stub
 	}
 	private void changeDirection() {
 		setDy(getDy() * -1);
