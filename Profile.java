73b936e44a5dbf81eebf2129719c6527bdfc94f1
gsims75 <57875934+gsims75@users.noreply.github.com>
2019-12-08 17:44:01 +0000

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -1,14 +1,23 @@
+package application;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-
+/**
+ * 
+ * @author Radoslav
+ */
 public class Profile {
     private String name;
     private int highestLevel = 0;
 
     private static HashMap<String, Integer> mapProfiles = new HashMap<String, Integer>();
-
+    /**
+     * Profile constructor 
+     * @param name The name of the profile that is being made
+     * @param highestLevel The profiles highest level 
+     * @throws IOException 
+     */
     public Profile(String name, int highestLevel) throws IOException {
         this.name = name;
         setHighestLevel(highestLevel);
@@ -26,12 +35,18 @@ public class Profile {
         }
 
     }
-
+    /**
+     * Method to set the highest Level of the profile 
+     * @param nLevel Integer that represents the highest level reached
+     */
     public void setHighestLevel(int nLevel) {
 
         this.highestLevel = nLevel;
     }
-
+    /**
+     * Simple return of the highest Level
+     * @return returns a integer that represents the profiles highest level
+     */
     public int getHighestLevel() {
 
         return this.highestLevel;
@@ -41,6 +56,11 @@ public class Profile {
         System.out.println("Profile is loaded successfully!");
     }
 
+    /**
+     * Method to delete a profile 
+     * @param name String representing the name of the profile that is to be deleted 
+     * @throws IOException
+     */
     public static void deleteProfile(String name) throws IOException {
         for (String name2 : mapProfiles.keySet()) {
             if (name.equals(name2)) {
@@ -56,7 +76,11 @@ public class Profile {
 
         fileWriter2.close();
     }
-
+    /**
+     * Method to read profiles from the file system and to add them into the gui profile list 
+     * @param in A scanner that has the name and the highest level reached by the profile 
+     * @throws IOException
+     */
     private static void readProfiles(Scanner in) throws IOException {
 
         while (in.hasNextLine()) {
@@ -71,7 +95,11 @@ public class Profile {
         in.close();
 
     }
-
+    /**
+     * Method to open the specified file
+     * @param filename A string that represents the filename
+     * @throws IOException If the file is not found a FileNotFoundException is thrown and handled 
+     */
     public static void readProfiles(String filename) throws IOException {
         File profileFile = new File(filename);
         Scanner in = null;
@@ -85,7 +113,10 @@ public class Profile {
 
         Profile.readProfiles(in);
     }
-
+    /**
+     * 
+     * @return
+     */
     public static String getProfiles() {
         for (String i : mapProfiles.keySet()) {
             System.out.println("Username: " + i + " Highest Level: " + mapProfiles.get(i));
